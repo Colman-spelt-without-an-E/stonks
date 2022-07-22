2acb7b1ed78a9ef3e176e367d665c001f98586a8
Colman-spelt-without-an-E <leekayin2003@gmail.com>
2022-07-22 22:00:48 +0100

    Update

--- framework.py
+++ framework.py
@@ -1,103 +1,124 @@
-import numpy as np
-
 class TradingBotPaper:
     """
-    A class representing a trading bot on paper with analysis tools. 
-    
+    A class representing a trading bot on paper with analysis tools.
+
     Parameters
     ----------
-    base_wealth : float
-                  amount of wealth starting off with
-    p_history : list or numpy array
-                (known) prices at each time interval up until present, non-inclusive
-    p_future : numpy array
-               prices at each time interval starting from present inclusive
-    asset : float
-            units of asset currently in possession
+    capital : float
+              capital starting off with
+    share : float
+            shares possessed starting off
+    past : list
+           (known) past time series up until present, non-inclusive;
+           used to determine action buy/sell
+    future : list
+             future time series, present inclusive;
+             used to determine action buy/sell
+    future_price : list
+                   future share prices, present inclusive;
+                   used to compute capital, shareholder value, and net worth
     """
-    def __init__(self, base_wealth, p_history, p_future, asset=0):
+
+    def __init__(self, past, future, future_price=None, capital=0, share=0):
         """
-        Construct attributes. 
-        
+        Construct attributes.
+
         Attributes
         ----------
+        t : int
+            present time index
+        x : float
+            present time series entry
         p : float
             current price
-        
-        trade_history : list
-                        stores historic trading outcomes of each day
-        
-        asset_wealth : float
-                       asset wealth
-        asset_history : list
-                        stores historic asset
-        asset_wealth_history : list
-                               stores historic asset wealth after each trade
-                        
-        liquid_wealth : float
-                        liquid wealth
-        liquid_wealth_history : list
-                                stores historic liquid wealth after each trade
-                                
-        total_wealth : float
-                       total wealth, including liquidity and asset; 
-                       ie. how much you have in total if you sell all assets at present
-        total_wealth_history : list
-                               stores historic total wealth after each trade        
-                               
-        outcome : float
-              units to buy/sell;
-                  positive means to buy, negative means to sell
-        t : int
-            present time index, usually in days
+
+        past : list
+               (known) past time series up until present, non-inclusive
+        future : list
+                 future time series, present inclusive;
+                 used to determine action buy/sell
+        future_price : list
+                       future share prices, present inclusive;
+                       used to compute capital, shareholder value, and net worth
+
+        buy_history : list
+                      stores historical buys each day;
+                      positive buys, negative sells
+
+        capital : float
+                  capital
+        capital_history : list
+                          stores historical capital
+
+        share : float
+                share possessed
+        share_history : float
+                        stores historical shares possessed
+
+        shareholder_val : float
+                          shareholder value
+        shareholder_val_history : list
+                                  stores historical shareholder values
+
+        net_worth : float
+                    net worth;
+                    ie. how much you have in total if you sell all assets at present
+        net_worth_history : list
+                            stores historical net worth
         """
-        
+
         self.t = 0
-        self.p_future = p_future
-        self.p = self.p_future[self.t]
-        self.p_history = p_history
-        
-        self.trade_history = []
-        
-        self.liquid_wealth = self.total_wealth = base_wealth
-        self.liquid_wealth_history = [self.liquid_wealth]
-        
-        self.asset = asset
-        self.asset_history = [self.asset]
-        
-        self.asset_wealth = asset * self.p
-        self.asset_wealth_history = [self.asset_wealth]
-        
-        self.total_wealth = self.liquid_wealth + self.asset_wealth
-        self.total_wealth_history = [self.total_wealth]
-        
-    def buy_sell(self):
+        self.x = future[0]
+        self.past = past
+        self.future = future
+        if future_price is None:
+            self.future_price = self.future
+        else:
+            self.future_price = future_price
+        self.p = self.future_price[0]
+
+        self.buy_history = []
+
+        self.capital = capital
+        self.capital_history = [self.capital]
+
+        self.share = share
+        self.share_history = [self.share]
+
+        self.shareholder_val = self.share * self.p
+        self.shareholder_val_history = [self.shareholder_val]
+
+        self.net_worth = self.shareholder_val + self.capital
+        self.net_worth_history = [self.net_worth]
+
+    def buy(self):
         # overwrite this
         pass
-    
-    def trade(self):
+
+    def _update(self):
         "With current price p per unit of asset, update attributes after buying or selling, depending on buy_sell"
-        outcome = self.buy_sell()
-        self.asset += outcome
-        self.liquid_wealth -= outcome * self.p
-        self.asset_wealth = self.asset * self.p
-        self.total_wealth = self.liquid_wealth + self.asset_wealth
-        
-        # update time and price
+        outcome = self.buy()
+        self.share += outcome
+        self.capital -= outcome * self.p
+        self.shareholder_val = self.share * self.p
+        self.net_worth = self.capital + self.shareholder_val
+
+        # update time, time series entry, and price
         self.t += 1
-        self.p = self.p_future[self.t]
-        
+        self.x = self.future[self.t]
+        self.p = self.future_price[self.t]
+
         # update histories
-        self.trade_history.append(outcome)
-        self.p_history.append(self.p)
-        self.liquid_wealth_history.append(self.liquid_wealth)
-        self.asset_history.append(self.asset)
-        self.asset_wealth_history.append(self.asset_wealth)
-        self.total_wealth_history.append(self.total_wealth)
-        
+        self.buy_history.append(outcome)
+        self.past.append(self.x)
+        self.capital_history.append(self.capital)
+        self.share_history.append(self.share)
+        self.shareholder_val_history.append(self.shareholder_val)
+        self.net_worth_history.append(self.net_worth)
+
     def simulate(self, T):
         """
         Perform trades at every time interval for T times.
         """
         while self.t < T:
-            self.trade()
+            self._update()
--- strategies.py
+++ strategies.py
@@ -1,77 +1,12 @@
 import numpy as np
 from framework import TradingBotPaper
 
-class MA_MR_naive(TradingBotPaper):
-    """
-    Child class representing a naive mean reversion strategy using moving average,
-    ie. buy 1 unit when current price < moving average, sell 1 unit when current price > moving average
 
-    Parameters
-    ----------
-    order : int
-            order of simple moving average model
-    weight_type : str
-                  weight type of moving average model;
-                  "simple" for simple moving average model, "exp" for exponential moving average model
-    a : float
-        degree of weighting decrease of exponential moving average model;
-        between 0 and 1
-    *args
-        passed to TradingBotPaper class
-    **kwargs
-        passed to TradingBotPaper class
-    """
-
-    def __init__(self, order=5, a=0.125, weight_type="simple", *args, **kwargs):
-        """
-        Create attributes.
-
-        Attributes
-        ----------
-        ma : float
-             current moving average, does not include current price
-        ma_history : list
-                     stores the historic moving averages starting from p_history for exponential moving average model;
-                     stores the historic moving averages starting from present for simple moving average model
-        """
-        super().__init__(*args, **kwargs)
-        self.weight_type = weight_type
-
-        if self.weight_type == "simple":
-            self.order = order
-            self.ma_history = []
-
-        if self.weight_type == "exp":
-            self.a = a
-            self.ma = self.p_history[0]
-            self.ma_history = [self.ma]
-            for p in self.p_history[1:-1]:
-                self.ma = self.a * p + (1 - self.a) * self.ma
-                self.ma_history.append(self.ma)
-
-    def buy_sell(self):
-        # compute moving average
-        if self.weight_type == "simple":
-            window = self.p_history[-self.order:]
-            self.ma = np.mean(window)
-
-        elif self.weight_type == "exp":
-            self.ma = self.a * self.p_history[-1] + (1 - self.a) * self.ma
-
-        self.ma_history.append(self.ma)
-
-        if self.p < self.ma:
-            return 1
-        elif self.p == self.ma:
-            return 0
-        else:
-            return -1
-        
 class MA_MR(TradingBotPaper):
     """
-    Child class representing a mean reversion strategy using moving average, 
-    with additional control on units to buy depending on the difference between current price and moving average. 
-    
+    Child class representing a mean reversion strategy using moving average,
+    with additional control on units to buy depending on the difference between current price and moving average.
+
     Parameters
     ----------
     order : int
@@ -80,23 +15,24 @@ class MA_MR(TradingBotPaper):
         degree of weighting decrease of exponential moving average model;
         between 0 and 1
     weight_type : str
-                  weight type of moving average model; 
+                  weight type of moving average model;
                   "simple" for simple moving average model, "exp" for exponential moving average model
     buy_func : str or function
                function of one argument;
                takes the difference between current price and moving average and returns the unit of shares to buy/sell,
-               positive buys, negative sells; 
-               default "ind", representing the indicator function of 
+               positive buys, negative sells;
+               default "sgn", representing the sign function of
                buying 1 share when current price < moving average, and selling 1 vice versa
     *args
         passed to TradingBotPaper class
     **kwargs
         passed to TradingBotPaper class
     """
-    def __init__(self, order=5, a=0.125, weight_type="simple", buy_func="ind", *args, **kwargs):
+
+    def __init__(self, order=5, a=0.125, weight_type="simple", buy_func="sgn", *args, **kwargs):
         """
         Create attributes.
-        
+
         Attributes
         ----------
         ma : float
@@ -108,40 +44,40 @@ class MA_MR(TradingBotPaper):
         super().__init__(*args, **kwargs)
         self.weight_type = weight_type
         self.buy_func = buy_func
-        
+
         if self.weight_type == "simple":
             self.order = order
-            self.ma_history=[]
-        
+            self.ma_history = []
+
         if self.weight_type == "exp":
             self.a = a
-            self.ma = self.p_history[0]
+            self.ma = self.past[0]
             self.ma_history = [self.ma]
-            for p in self.p_history[1:-1]:
-                self.ma = self.a * p + (1 - self.a) * self.ma
+            for x in self.past[1:-1]:
+                self.ma = self.a * x + (1 - self.a) * self.ma
                 self.ma_history.append(self.ma)
-        
-    def buy_sell(self): 
+
+    def buy(self):
         # compute moving average
         if self.weight_type == "simple":
-            window = self.p_history[-self.order:]
+            window = self.past[-self.order:]
             self.ma = np.mean(window)
-            
+
         elif self.weight_type == "exp":
-            self.ma = self.a * self.p_history[-1] + (1 - self.a) * self.ma
-        
+            self.ma = self.a * self.past[-1] + (1 - self.a) * self.ma
+
         self.ma_history.append(self.ma)
-        
-        if self.buy_func == "ind":        
-            if self.p < self.ma:
+
+        if self.buy_func == "sgn":
+            if self.x < self.ma:
                 return 1
-            elif self.p == self.ma:
+            elif self.x == self.ma:
                 return 0
             else:
                 return -1
-        
+
         if callable(self.buy_func):
-            d = self.ma - self.p
+            d = self.ma - self.x
             return self.buy_func(d)
         else:
             raise TypeError("buy_func not callable")
